3f861a0ddf79b4e2caa308edcf53af327d25f34c
Famous <famousoko@gmail.com>
2017-11-07 17:28:36 +0100

    created custom adapter

--- app/src/main/java/com/giantstrideltd/coderswag/Model/Category.kt
+++ app/src/main/java/com/giantstrideltd/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.giantstrideltd.coderswag.Model
 /**
  * Created by Famous Oko on 11/7/2017.
  */
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String){
+    override fun toString(): String {
+        return title
+    }
+}
